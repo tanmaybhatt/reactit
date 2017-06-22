d78a8f2cba47f09a66ef5b9635a46e2e9c3a17f0
Tanmay Bhatt <Tanmay Bhatt>
2017-06-22 21:58:41 +0530

    Express server added

--- bin/root.js
+++ bin/root.js
@@ -1,11 +1,14 @@
 #! /usr/bin/env node
 const chalk = require('chalk');
 var args = process.argv;
-if(args.indexOf('new')>-1)
+if(args.indexOf('new')==2)
 require("../lib/new.js");
-else if(args.indexOf('start')>-1)
+
+else if(args.indexOf('start')==2)
 require("../lib/start.js");
-else if(args.indexOf('build')>-1)
+
+else if(args.indexOf('build')==2)
 require("../lib/build.js");
+
 else 
 console.log("No Arguments");
